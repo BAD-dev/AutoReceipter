2ad5dbce975d9e347144fef3e243b64a6883c731
BAD-dev <jbonnells@knights.ucf.edu>
2016-03-23 21:03:12 -0400

    ScanScreen
    Template for now

---
+++ core/src/com/github/autoreceipter/ScanScreen.java
@@ -0,0 +1,39 @@
+package com.github.autoreceipter;
+
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+/**
+ * Created by Julian on 3/22/2016.
+ */
+public class ScanScreen extends BaseScreen {
+
+    public ScanScreen(final AutoReceipter app) {
+        super(app);
+
+        final ImageButton backButton = new ImageButton(app.skin.get("backButtonStyle", ImageButton.ImageButtonStyle.class));
+
+        Label label = new Label("Scan Screen", app.skin);
+
+        table.defaults().pad(6f);
+        table.add(label);
+        table.row();
+        table.add(backButton);
+
+        backButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                app.switchScreens(new MainMenuScreen(app));
+                backButton.setChecked(false);
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPress() {
+        app.switchScreens(new MainMenuScreen(app));
+    }
+}

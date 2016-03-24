3b9bcd552a385959a4c066f81a32039c46018bb5
BAD-dev <jbonnells@knights.ucf.edu>
2016-03-23 21:02:23 -0400

    BaseScreen
    Abstract class for all other screens. Includes main table and abstract
    Screen methods

---
+++ core/src/com/github/autoreceipter/BaseScreen.java
@@ -0,0 +1,46 @@
+package com.github.autoreceipter;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
+/**
+ * Created by Julian on 3/22/2016.
+ */
+public abstract class BaseScreen extends Group {
+
+    protected final AutoReceipter app;
+    protected final Table table = new Table();
+    protected final AssetManager assetManager = new AssetManager();
+
+    public static float padSize;
+    public float screenDuration;
+
+    public abstract void onBackPress();
+
+    public BaseScreen(AutoReceipter app) {
+        this.app = app;
+        padSize = Math.round(Math.max(app.width, app.height)*.02f);
+
+        table.defaults().pad(padSize);
+        table.setSize(app.width, app.height);
+        this.addActor(table);
+    }
+
+    public BaseScreen show() {
+        return this;
+    }
+
+    public void screenTransition() {
+        float x = -app.width;
+        float duration = .333f;
+
+        MoveToAction action = Actions.moveTo(x, 0f, duration);
+        addAction(action);
+    }
+
+    public void hide() { }
+}

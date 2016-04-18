0a44d69bac70de9d5afc2532e8dc962e4e3b112d
BAD-dev <jbonnells@knights.ucf.edu>
2016-04-18 16:15:18 -0400

    Updated list screen and added List Item class

---
+++ core/src/com/github/autoreceipter/ListItem.java
@@ -0,0 +1,17 @@
+package com.github.autoreceipter;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.Widget;
+
+/**
+ * Created by Julian on 4/16/2016.
+ */
+public class ListItem extends Widget {
+
+    public static float widgetWidth, widgetHeight;
+    public Table widget;
+
+    public ListItem() {
+
+    }
+}

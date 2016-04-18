5719cb0471f54a62074021b6717a3e52c5e8eb28
Ganemo <ganemogiannis@knights.ucf.edu>
2016-04-18 16:29:37 -0400

    Launcher Modification

--- android/src/com/github/autoreceipter/AndroidLauncher.java
+++ android/src/com/github/autoreceipter/AndroidLauncher.java
@@ -1,12 +1,23 @@
 package com.github.autoreceipter;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.github.autoreceipter.AutoReceipter;
+import com.github.autoreceipter.ocr.CameraController;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AndroidLauncher extends AndroidApplication {
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -14,6 +25,11 @@ public class AndroidLauncher extends AndroidApplication {
 		config.useAccelerometer = false;
         config.useCompass = false;
 
-        initialize(new TestApp(), config);
+
+		CameraController pic = new PictureTaker(this);
+
+		initialize(new TestApp(pic), config);
+		//dispatchTakePictureIntent();
 	}
+
 }

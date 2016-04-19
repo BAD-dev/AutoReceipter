3dae83b072867a8ac6450c02b5dfe47867468665
Ganemo <ganemogiannis@knights.ucf.edu>
2016-04-19 00:40:37 -0400

    Post-Scan list commit and shoppingScreen init

--- core/src/com/github/autoreceipter/ListItem.java
+++ core/src/com/github/autoreceipter/ListItem.java
@@ -1,17 +1,42 @@
 package com.github.autoreceipter;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.Widget;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.github.autoreceipter.ocr.ImageProcessing;
 
 /**
  * Created by Julian on 4/16/2016.
  */
-public class ListItem extends Widget {
+public class ListItem extends FridgeItem {
 
-    public static float widgetWidth, widgetHeight;
-    public Table widget;
+    public CheckBox checkBox;
+    public boolean checked = true;
 
-    public ListItem() {
+    public ListItem(String name, String description, Color color, Skin skin) {
+        super(name, description, color, skin);
 
+        checkBox = new CheckBox("", skin);
+        checkBox.getCells().get(0).size(100, 100);
+        checkBox.setChecked(true);
+        checkBox.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                if(checked)
+                    checked = false;
+                else
+                    checked = true;
+
+                checkBox.setChecked(checked);
+            }
+        });
+
+        widget.add(checkBox).padLeft(20).right();
     }
 }
--- core/src/com/github/autoreceipter/ListScreen.java
+++ core/src/com/github/autoreceipter/ListScreen.java
@@ -1,9 +1,18 @@
 package com.github.autoreceipter;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 import com.badlogic.gdx.utils.ArrayMap;
+
+import java.util.ArrayList;
 
 /**
  * Created by Julian on 3/23/2016.
@@ -13,31 +22,57 @@ import com.badlogic.gdx.utils.ArrayMap;
 public class ListScreen extends BaseScreen {
 
     // List of items in our shopping list
-    private ArrayMap<Integer, ListItem> list;
+    private ArrayList<ListItem> list;
 
-    public ListScreen(final AutoReceipter app) {
+    Table scrollTable;
+
+    public ListScreen(final AutoReceipter app, final ArrayList<FridgeItem> items) {
         super(app);
 
         //final ImageButton backButton = new ImageButton(app.skin.get("backButtonStyle", ImageButton.ImageButtonStyle.class));
 
         Label label = new Label("Shopping List Screen", app.skin);
-        CheckBox checkBox = new CheckBox("Sample Checkbox", app.skin);
 
         table.defaults().pad(6f);
         table.setBackground(new NinePatchDrawable(getNinePatch("background/background_white_noheader.png")));
         table.add(label).row();
-        table.add(checkBox).row();
 
         table.debug();
-        //table.add(backButton);
 
-        /*backButton.addListener(new ClickListener() {
+        scrollTable = new Table();
+        final ScrollPane scrollPane = new ScrollPane(scrollTable);
+
+        // vertical only scrolling
+        scrollPane.setScrollingDisabled(true, false);
+
+        FridgeItem.setDimensions(app.width, app.height);
+
+        generateListItems(items);
+
+
+        ImageButton fridgeButton = new ImageButton(app.skin.get("fridgeButtonStyle", ImageButton.ImageButtonStyle.class));
+
+        fridgeButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                app.switchScreens(new MainMenuScreen(app));
-                backButton.setChecked(false);
+                for(int i=0; i<list.size(); i++)
+                    if(list.get(i).checked)
+                        app.items.add(items.get(i));
+                app.switchScreens(new FridgeScreen(app));
             }
-        });*/
+        });
+
+        scrollTable.add(fridgeButton);
+
+        table.add(scrollPane);
+    }
+
+    public void generateListItems(ArrayList<FridgeItem> items) {
+        for(int x=0; x<items.size(); x++) {
+            Color c = new Color(MathUtils.random(0.5f), MathUtils.random(0.5f), MathUtils.random(0.5f), 1f);
+            ListItem o = new ListItem(items.get(x).getItemName(), "Item "+ x, c, app.skin);
+            scrollTable.add(o.widget).expandX().prefHeight(300f).padLeft(35f).left().row();
+        }
     }
 
     @Override

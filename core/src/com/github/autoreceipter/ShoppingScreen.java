e60ba076ed0102f08a3abdb4a6bdbe52a515d5e9
Ganemo <ganemogiannis@knights.ucf.edu>
2016-04-19 15:44:07 -0400

    Revert "Revert "Revert "Revert "Revert "Revert "Shopping screen changes""""""
    This reverts commit 1c64f3e1fb511412d34560cf36fc342ec8483307.

--- core/src/com/github/autoreceipter/ShoppingScreen.java
+++ core/src/com/github/autoreceipter/ShoppingScreen.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by Georg on 4/18/2016.
@@ -90,6 +91,8 @@ public class ShoppingScreen extends BaseScreen {
                         for (int i = 0; i < listItems.size(); i++) {
                             if (listItems.get(i).checked) {
                                 items.remove(i);
+                                listItems.remove(i);
+                                i--;
                             }
                         }
 
@@ -159,6 +162,8 @@ public class ShoppingScreen extends BaseScreen {
                     for (int i = 0; i < listItems.size(); i++) {
                         if (listItems.get(i).checked) {
                             items.remove(i);
+                            listItems.remove(i);
+                            i--;
                         }
                     }
 
@@ -185,9 +190,14 @@ public class ShoppingScreen extends BaseScreen {
     }
 
     public void generateAutomaticList() {
+        ArrayList<FridgeItem> list = new ArrayList<FridgeItem>();
         for(int x=0; x<app.items.size(); x++) {
-            //if(app.items.get(x).getLastPurchased())
+            long daysBetween = (new Date().getTime() - app.items.get(x).lastPurchased.getTime()) / (1000*60*60*24);
+            if(daysBetween > 7) {
+                list.add(app.items.get(x));
+            }
         }
+        addItemsToTable(list);
     }
 
     @Override
